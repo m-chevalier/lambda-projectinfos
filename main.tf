d1761963532c06cad0dcb534d1ea489799aab7d7
Alix Peigue <a.peigue@groupeonepoint.com>
2023-08-04 11:15:16 +0200

    Added lambda invoke permission

--- main.tf
+++ main.tf
@@ -40,8 +40,18 @@ data "aws_iam_policy_document" "projectinfo_lambda_policy" {
 
 resource "aws_iam_role" "projectinfo_lambda_iam" {
   name                  = "limited-projectinfo_lambda_iam"
-  permissions_boundary  = "arn:aws:iam::135225040694:policy/SysopsPermissionsBoundary" 
+  //permissions_boundary  = "arn:aws:iam::135225040694:policy/SysopsPermissionsBoundary" 
   assume_role_policy    = data.aws_iam_policy_document.projectinfo_lambda_policy.json
+}
+
+data "aws_organizations_organization" "org" {}
+
+resource "aws_lambda_permission" "allow_organization_invoke" {
+  statement_id = "AllowOrganizationInvoke"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.project_infos.function_name
+  principal = "*"
+  principal_org_id = data.aws_organizations_organization.org.id
 }
 
 // - Lambda
